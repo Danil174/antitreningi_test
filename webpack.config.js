1d157d900edcc4bc1628dfb51747fc515b0a873d
danil milostiviy <milostiviy.danil@yandex.ru>
2020-10-18 23:12:08 +0300

    fix proxy for fetch

--- webpack.config.js
+++ webpack.config.js
@@ -44,10 +44,8 @@ module.exports = {
     port: 4200,
     hot: isDev,
     historyApiFallback: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    proxy: {
+      '/api': 'http://localhost:3000'
     }
   },
   devtool: isDev ? 'source-map' : '',
